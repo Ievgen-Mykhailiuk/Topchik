41009798a6f0f6e99e6e5898a3df8863637a3be3
IevgenMykhailiuk <ievgen.mykhailiuk@gmail.com>
2023-11-28 18:00:27 +0100

    organised project folders

--- Topchik/Topchik/App/TopchikApp.swift
+++ Topchik/Topchik/App/TopchikApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TopchikApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
         }
     }
 }
--- Topchik/Topchik/Modules/List/View/MainListView.swift
+++ Topchik/Topchik/Modules/List/View/MainListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainListView.swift
 //  Topchik
 //
 //  Created by Евгений  on 28/11/2023.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainListView: View {
     var body: some View {
         VStack {
             Image(systemName: Images.pin.name)
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainListView()
     }
 }
